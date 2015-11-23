c28c697cf4c1de5d110dd9a2e162cd3fa46ba5a9
Christine Cha <hello@christinecha.com>
2015-11-23 14:51:11 -0500

    Created mailinglist sign up (temporary) instead of leading to login

--- public/js/home.js
+++ public/js/home.js
@@ -1,7 +1,26 @@
+$('.mailinglist-container').hide();
+
 $('#search').on('submit', function(e){
   e.preventDefault();
-  var query = $("#query").val();
-  var time = $("#query-time").val();
-  location.href = "/search" + query + time;
+  // var query = $("#query").val();
+  // var time = $("#query-time").val();
+  // location.href = "/search" + query + time;
+
+  // temporarily redirects to mailing list sign up
+  $('.search-container').hide();
+  $('.mailinglist-container').show();
+  return false;
+});
+
+$('#mailinglist').on('submit', function(e){
+  e.preventDefault();
+  var email = $('#email').val();
+  ref.child('mailinglist').push({
+    email:  email,
+    addedAt: Firebase.ServerValue.TIMESTAMP,
+    for: 'closed beta',
+  });
+  $('.confirmation-container').html('<h4>Yay! You\'ve been added to the list.</h4>');
+  $('#email').val('');
   return false;
 });
