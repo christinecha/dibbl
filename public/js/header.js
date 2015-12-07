82f8cf5720e41e0c1ddd3e70117c8011f116b80b
Christine Cha <hello@christinecha.com>
2015-12-06 19:24:01 -0500

    Fixed auth header glitch, and updated mobile-opt for expert profile and header.

--- public/js/header.js
+++ public/js/header.js
@@ -6,13 +6,14 @@ $('#header').load('partials/header', function() {
       $('.secure').show();
       $('.public-only').hide();
       console.log("User " + authData.uid + " is logged in with " + authData.provider);
+      currentUserId = authData.uid;
       usersRef.child(authData.uid).on("value", function(snapshot) {
         currentUser = snapshot.val();
-        currentUserId = snapshot.key();
       });
       var user = new User();
       user.checkAvailability(authData.uid);
     } else {
+      $('.secure').hide();
       console.log("User is logged out");
       currentUser = null;
       currentUserId = null;
@@ -26,35 +27,33 @@ $('#header').load('partials/header', function() {
     }
   };
   ref.onAuth(authDataCallback);
+
+  $('#header').on('click', '.availability', function(){
+    var user = new User();
+    user.changeAvailability(currentUserId);
+  });
+
+  $('#header').on('click', '.account', function(){
+    $(this).siblings('.dropdown-account').toggle();
+  });
+
+  $('#header').on('click', '.account-dashboard', function(){
+    location.href = "/account?user=" + currentUserId + "&view=dashboard";
+  });
+
+  $('#header').on('click', '.account-inbox', function(){
+    location.href = "/account?user=" + currentUserId + "&view=inbox";
+  });
+
+  $('#header').on('click', '.logout', function(){
+    ref.unauth();
+    location.href = "/";
+  });
 });
+
 
 $('#footer').load('partials/footer', function() {
 });
-
-$('#header').on('click', '.availability', function(){
-  var user = new User();
-  user.changeAvailability(currentUserId);
-});
-
-$('#header').on('click', '.account', function(){
-  $(this).siblings('.dropdown-account').toggle();
-});
-
-$('#header').on('click', '.account-dashboard', function(){
-  location.href = "/account?user=" + currentUserId + "&view=dashboard";
-});
-
-$('#header').on('click', '.account-inbox', function(){
-  location.href = "/account?user=" + currentUserId + "&view=inbox";
-});
-
-$('#header').on('click', '.logout', function(){
-  ref.unauth();
-  location.href = "/";
-});
-
-
-
 
 
 
